b3b71317067d12c134a89ffc0fcffe1a3151d9da
arnold <adododjialban@gmail.com>
2025-12-01 16:49:59 +0000

    test 1 core

--- src/Components/Landing/Offres.jsx
+++ src/Components/Landing/Offres.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 const Offres = () => {
+    const navigate = useNavigate();
+  
   const [isAnnual, setIsAnnual] = useState(false);
 
   const plans = [
@@ -232,7 +236,9 @@ const Offres = () => {
         
       </div>
       <div className="flex flex-col py-22 sm:flex-row gap-4 justify-center items-center">
-          <button className="bg-gradient-to-r from-blue-600 to-pink-800 text-white font-semibold text-base px-6 py-3 rounded-lg hover:from-blue-700 hover:to-pink-900 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl w-full sm:w-auto">
+          <button 
+          onClick={() => navigate("/login")}
+          className="bg-gradient-to-r from-blue-600 to-pink-800 text-white font-semibold text-base px-6 py-3 rounded-lg hover:from-blue-700 hover:to-pink-900 transition-all duration-300 transform hover:scale-105 shadow-lg hover:shadow-xl w-full sm:w-auto">
             <span className="flex items-center justify-center gap-2">
               <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                </svg>
